1c95b3281c8731941fa3d8dd49ffe8163921dfa2
Percentpliadvance <drumgoleflavia@gmail.com>
2026-06-06 04:24:26 -0400

    Update BackupEngine.cpp

--- src/cpp/BackupEngine.cpp
+++ src/cpp/BackupEngine.cpp
@@ -1,1 +1,19 @@
+#include "BackupEngine.h"
+
+bool BackupEngine::CreateBackup(const std::string& sourcePath,
+const std::string& destinationPath)
+{
+return true;
+}
+
+bool BackupEngine::RestoreBackup(const std::string& backupPath,
+const std::string& restorePath)
+{
+return true;
+}
+
+std::vector[std::string](std::string) BackupEngine::GetAvailableBackups()
+{
+return {};
+}
 
