c233cd343594e1a8b6df3b809f783e37418b920f
Percentpliadvance <drumgoleflavia@gmail.com>
2026-06-06 04:25:24 -0400

    Create GameProfile.cs

---
+++ csharp/GameProfile.cs
@@ -0,0 +1,11 @@
+namespace UniversalSaveBackup
+{
+    public class GameProfile
+    {
+        public string GameName { get; set; }
+
+        public string SaveLocation { get; set; }
+
+        public string BackupLocation { get; set; }
+    }
+}
